2ecc3b61211180a5efff50aa952e5e8b219ee93c
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-05-23 17:57:59 -0700

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,10 @@
+#version one
 
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb (ending in 'ing'): ")
+verb2 = input("Enter a verb: ")
+noun2 = input("Enter a noun: ")
+
+madlibs = f"Hey there, welcome to my {noun1}, today we will be {verb1}, and riding our {verb2} with the great {noun2}"
+
+print(madlibs)
