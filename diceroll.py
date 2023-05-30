69ce6910d2c8a574e6acbb85f3f92438a558325b
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-05-29 19:47:00 -0700

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -1,1 +1,18 @@
+import random
+
+#dice rolling simulator
+
+def roll_dice():
+    dice_number = random.randint(1,6)
+    print("Enter 'done' to end the program")
+    user_input = input("Type 'roll' to roll the dice: ").lower()
+    print(f"You rolled {dice_number}")
+    if user_input == "done":
+        quit()
+    print("The program terminated successfully")
+
+
+roll_dice()
+
+
 
