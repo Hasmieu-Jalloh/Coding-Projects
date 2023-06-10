3e90eed96e4b87b622a78f13c7cade0da662670c
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-06-10 04:45:07 +0000

    Just completed the solution for the problem

--- oneDarraysum.py
+++ oneDarraysum.py
@@ -3,4 +3,14 @@ def sum(array):
     counter = 0
     summed_values = []
 
-    for i in range()
+    for i in range(array_len):
+        counter += array[i]
+        summed_values.append(counter)
+        i -= 1
+    return summed_values
+
+testing_array = [1,2,3,4,5,6,7,8,9]
+
+test = sum(testing_array)
+
+print(test)
