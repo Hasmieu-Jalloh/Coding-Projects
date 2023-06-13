e4a6ea319ad7cf21fa433452d4bd49c97976f133
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-06-13 20:30:58 +0000

    Just crated a not so intelligent chat bot

--- pgame.py
+++ pgame.py
@@ -1,8 +1,17 @@
 import pygame
+
 pygame.init()
 
 
 HEIGHT = 800
 WIDTH = 400
 
-display = pygame.display.set_mode((HEIGHT, WIDTH))
+screen = pygame.display.set_mode((HEIGHT, WIDTH))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    #Drawing all our elements
+    #Update everything
+    pygame.display.update()
