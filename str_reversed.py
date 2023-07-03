ee15c98324bd453bc9d273684e36c4b31706efd0
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-07-03 19:29:56 +0000

    Added a new algorithm 'str_reversed.py'

---
+++ str_reversed.py
@@ -0,0 +1,18 @@
+# Reversed string algorithm
+
+def reversed_string(string_input):
+    str_len = len(string_input) - 1
+    reversed_str = ""
+
+    for str_index in range(str_len + 1):
+        reversed_str += (string_input[str_len])
+        str_len -= 1
+
+    return reversed_str
+
+
+name = "Hasmieu"
+
+test = reversed_string(name)
+
+print(test)
