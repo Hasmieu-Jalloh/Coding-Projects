f7803c79ff9018234d77d26fdc8fdf376f889fcd
Hasmieu Jalloh <hazmieujay@gmail.com>
2023-07-09 04:35:11 +0000

    Done for today

--- intersection.py
+++ intersection.py
@@ -4,21 +4,21 @@ def intersection(arr1, arr2):
     # Function printing the intersections between two lists
     arr1_len = len(arr1)
     arr2_len = len(arr2)
-    new_values = []
+    intersected_values = []
     for indices1 in range(arr1_len):
         for indices2 in range(arr2_len):
             if arr1[indices1] == arr2[indices2]:
-                if arr1[indices1] in new_values:
+                if arr1[indices1] in intersected_values:
                     continue
                 else:
-                    new_values.append(arr1[indices1])
+                    intersected_values.append(arr1[indices1])
     
-    return new_values
+    return intersected_values
 
 
 data1 = [1,2,3,4,5]
 data2 = [3,5,6,7,8]
 
-test = intersection(data1, data2)
+intersect = intersection(data1, data2)
 
-print(test)
+print(intersect)
